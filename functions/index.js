d18574f3a0a5271e92b9b14e146a506c521843fb
KinFol <Kinfol>
2019-10-13 15:44:47 +0100

    add different urls to loop over and"

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,18 @@ const request = require('request-promise');
 const endpoint = "https://api.github.com/users";
 const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36';
 
+const urls = [
+    `${endpoint}/kinfol`,
+    `${endopint}/kinfol/repos`,
+    `${endpoint}/kinfol/repos`
+];
+
 (async function main() {
     try {
+
+    // loop over and call all the endpoints 
+
+    // do something else when we're done
         const res = await request({
             url: `${endpoint}/kinfol`,
             method: 'GET',
